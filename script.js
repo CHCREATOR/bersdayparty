70533a5677b807ce6a119500c897649c3b865c3f
CHCREATOR <charlesjeux2019@outlook.fr>
2025-04-26 14:25:40 +0200

    Update script.js

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ function updateCountdown() {
     const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    countdown.innerHTML = `${days}j ${hours}h ${minutes}m ${seconds}s`;
+    countdown.innerHTML = `${days}J  .  ${hours}H  .  ${minutes}M  .  ${seconds}S`;
 }
 setInterval(updateCountdown, 1000);
 
